dd8548121b7dfb40cc354e1dade80bad760933a1
Gonçalo Malheiro <malheirogoncalo9@gmail.com>
2019-06-12 11:35:59 +0100

    burger king

--- registo00-master/app.py
+++ registo00-master/app.py
@@ -1,71 +1,97 @@
 from flask import Flask, render_template, request
+import psycopg2
 
 app = Flask(__name__)
 
+def herokudb():
+    Host='ec2-54-247-72-30.eu-west-1.compute.amazonaws.com'
+    Database='dfp9ni1rfj79li'
+    User='bfsgffjyfxafko'
+    Password='8fcd565921915578936c493b3db9ac3a622996e354e12a4df7c4d9b73a717db0'
+    return psycopg2.connect(host=Host, database=Database,user=User, password=Password, sslmode='require')
 
-def gravar(v1, v2, v3):
-    import sqlite3
-    ficheiro = sqlite3.connect('db/Utilizadores.db')
+def gravar(v1, v2):
+    ficheiro = herokudb()
     db = ficheiro.cursor()
-    db.execute("CREATE TABLE IF NOT EXISTS usr (nome text,email text, passe text)")
-    db.execute("INSERT INTO usr VALUES (?, ?, ?)", (v1, v2, v3))
+    db.execute("CREATE TABLE IF NOT EXISTS usr (usr text, pwd text)")
+    db.execute("INSERT INTO usr VALUES ( %s, %s)", (v1, v2))
     ficheiro.commit()
     ficheiro.close()
-
-
-def existe(v1):
-    import sqlite3
-    ficheiro = sqlite3.connect('db/Utilizadores.db')
-    db = ficheiro.cursor()
-    db.execute("SELECT * FROM usr WHERE nome = ?", (v1,))
-    valor = db.fetchone()
-    ficheiro.close()
-    return valor
-
-
-def log(v1, v2):
-    import sqlite3
-    ficheiro = sqlite3.connect('db/Utilizadores.db')
-    db = ficheiro.cursor()
-    db.execute("SELECT * FROM usr WHERE nome = ? and passe = ?", (v1, v2,))
-    valor = db.fetchone()
-    ficheiro.close()
-    return valor
-
-
-@app.route('/registo', methods=['GET', 'POST'])
-def route():
-    erro = None
-    if request.method == 'POST':
-        v1 = request.form['utilizador']
-        v2 = request.form['email']
-        v3 = request.form['passe']
-        v4 = request.form['cpasse']
-        if existe(v1):
-            erro = 'O Utilizador já existe.'
-        elif v3 != v4:
-            erro = 'A palavra passe não coincide.'
-        else:
-            gravar(v1, v2, v3)
-    return render_template('registo.html', erro=erro)
+    return
 
 
 def alterar(v1, v2):
-    import sqlite3
-    ficheiro = sqlite3.connect('db/Utilizadores.db')
+    ficheiro = herokudb()
     db = ficheiro.cursor()
-    db.execute("UPDATE usr SET passe = ? WHERE nome = ?", (v2, v1))
+    db.execute("UPDATE usr SET pwd = %s WHERE usr = %s", (v2, v1))
     ficheiro.commit()
     ficheiro.close()
+    return
 
 
-def apaga(v1):
-    import sqlite3
-    ficheiro = sqlite3.connect('db/Utilizadores.db')
+def existe(v1):
+    try:
+        import sqlite3
+        ficheiro = sqlite3.connect('db/Utilizador.db')
+        db = ficheiro.cursor()
+        db.execute("SELECT * FROM usr WHERE usr = %s", (v1,))
+        valor = db.fetchone()
+        ficheiro.close()
+    except:
+        valor = None
+    return valor
+
+
+def eliminar(v1):
+    ficheiro = herokudb()
     db = ficheiro.cursor()
-    db.execute("DELETE FROM usr WHERE nome = ?", (v1))
+    db.execute("DELETE FROM usr WHERE usr = %s", (v1,))
     ficheiro.commit()
     ficheiro.close()
+    return
+
+
+def log(v1, v2):
+    ficheiro = herokudb()
+    db = ficheiro.cursor()
+    db.execute("SELECT * FROM usr WHERE usr = %s and pwd = %s", (v1, v2,))
+    valor = db.fetchone()
+    ficheiro.close()
+    return valor
+
+
+@app.route('/newpasse', methods=['POST', 'GET'])
+def newpasse():
+    erro = None
+    if request.method == "POST":
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        v3 = request.form['cpwd']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif v2 != v3:
+            erro = 'A palavra passe não coincide.'
+        else:
+            alterar(v1, v2)
+            erro = 'A palavra passe foi alterada.'
+    return render_template('newpasse.html', erro=erro)
+
+
+@app.route('/registo', methods=['POST', 'GET'])
+def registo():
+    erro = None
+    if request.method == "POST":
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        v3 = request.form['cpwd']
+        if existe(v1):
+            erro = 'O Utilizador já existe.'
+        elif v2 != v3:
+            erro = 'A palavra passe não coincide.'
+        else:
+            gravar(v1, v2)
+            erro = 'Utilizador registado com Sucesso.'
+    return render_template('registo.html', erro=erro)
 
 
 @app.route('/')
@@ -73,51 +99,36 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     erro = None
-    if request.method == 'POST':
-        v1 = request.form['utilizador']
-        v2 = request.form['passe']
+    if request.method == "POST":
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
         if not existe(v1):
             erro = 'O Utilizador não existe.'
         elif not log(v1, v2):
-            erro = 'A palavra passe está errada.'
+            erro = 'A senha está incorreta.'
         else:
-            erro = 'Bem-Vindo.'
+            erro = 'Bem-vindo.'
     return render_template('login.html', erro=erro)
 
 
-@app.route('/apagar', methods=['GET', 'POST'])
+@app.route('/apagar', methods=['POST', 'GET'])
 def apagar():
     erro = None
-    if request.method == 'POST':
-        v1 = request.form['utilizador']
-        v2 = request.form['passe']
+    if request.method == "POST":
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
         if not existe(v1):
             erro = 'O Utilizador não existe.'
         elif not log(v1, v2):
-            erro = 'A palavra passe está errada.'
+            erro = 'A senha está incorreta.'
         else:
-            apaga(v1)
-            erro = 'Conta Eliminada com Sucesso.'
+            eliminar(v1)
+            erro = 'Conta eliminada com Sucesso.'
     return render_template('apagar.html', erro=erro)
 
-
-@app.route('/newpasse', methods=['GET', 'POST'])
-def newpasse():
-    erro = None
-    if request.method == 'POST':
-        v1 = request.form['utilizador']
-        v2 = request.form['passe']
-        v3 = request.form['cpasse']
-        if not existe(v1):
-            erro = 'O Utilizador não existe.'
-        elif v2 != v3:
-            erro = 'A palavra passe não coincide.'
-        else:
-            alterar(v1, v2)
-    return render_template('newpasse.html', erro=erro)
 
 
 if __name__ == '__main__':
